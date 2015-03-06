aa3386ff243b3dad86f9f5d671ce35f897245dd1
Julien Guanzon <julienguanzon@gmail.com>
2015-03-06 16:15:25 -0700

    Adds navigation stack
    Signed-off-by: Julien Guanzon <julienguanzon@gmail.com>

---
+++ GetMeUTA/NowViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  GetMeUTA
+//
+//  Created by Taylor Mott on 6.3.2015.
+//  Copyright (c) 2015 Shelley-Guanzon-Henrie-Mott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NowViewController : UIViewController
+
+
+@end
+
