2f13b46855966e9490938008dcf96e70d94789e9
Paul Shelley <p.d.shelley@me.com>
2015-03-07 19:02:27 -0700

    Added picker view to the plan view

--- GetMeUTA/PlanViewController.m
+++ GetMeUTA/PlanViewController.m
@@ -8,7 +8,9 @@
 
 #import "PlanViewController.h"
 
-@interface PlanViewController ()
+@interface PlanViewController () <UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (nonatomic, strong) IBOutlet UIPickerView *fromPicker;
 
 @end
 
@@ -17,6 +19,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.fromPicker.delegate = self;
+    self.fromPicker.dataSource = self;
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,22 +30,67 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma picker - 
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return self.pickerData[component][row];
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return [self.pickerData count];
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return [self.pickerData[component] count];
+}
+
+- (NSArray *)pickerData {
+    
+    return @[
+             [self from],
+             [self to],
+             ];
+    
+}
+
+- (NSArray *)from {
+    return @[@"Pleasant View",
+             @"Ogden",
+             @"Roy",
+             @"Clearfield",
+             @"Layton",
+             @"Farmington",
+             @"Woods Cross",
+             @"North Temple",
+             @"Salt Lake Central",
+             @"Murray Central",
+             @"South Jordan",
+             @"Draper",
+             @"Lehi",
+             @"American Fork",
+             @"Orem",
+             @"Provo"];
+}
+
+- (NSArray *)to {
+    return @[@"Pleasant View",
+             @"Ogden",
+             @"Roy",
+             @"Clearfield",
+             @"Layton",
+             @"Farmington",
+             @"Woods Cross",
+             @"North Temple",
+             @"Salt Lake Central",
+             @"Murray Central",
+             @"South Jordan",
+             @"Draper",
+             @"Lehi",
+             @"American Fork",
+             @"Orem",
+             @"Provo"];
+}
+
 @end
 
-
-//Pleasant View Station
-//Ogden Intermodal Center
-//Roy Station
-//Clearfield Station
-//Layton Station
-//Farmington Station
-//Woods Cross Station
-//North Temple Station
-//Salt Lake Central Station
-//Murray Central Station
-//South Jordan Station
-//Draper Station
-//Lehi Station
-//American Fork Station
-//Orem Station
-//Provo Station
