4bd1f930e036aa22a701d5077f8a6416b82b9a19
Taylor Mott <trm36@humboldt.edu>
2015-03-12 10:52:01 -0600

    Sets NowViewController as rootViewController
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- GetMeUTA/AppDelegate.m
+++ GetMeUTA/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "NowViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,6 +19,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    self.window.rootViewController = [NowViewController new];
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
