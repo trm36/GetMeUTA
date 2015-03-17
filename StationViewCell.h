0beb6de160a73718ef88e16b5f4678bd0d3be512
Paul Shelley <p.d.shelley@me.com>
2015-03-17 14:44:51 -0600

    Adds JourneyView - Not Connected

---
+++ StationViewCell.h
@@ -0,0 +1,13 @@
+//
+//  StationViewCell.h
+//  GetMeUTA
+//
+//  Created by Paul Shelley on 3/17/15.
+//  Copyright (c) 2015 Shelley-Guanzon-Henrie-Mott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StationViewCell : UITableViewCell
+
+@end
