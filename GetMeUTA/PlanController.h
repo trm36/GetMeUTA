aa6d9104536c1520b2f5af17bf510c1dda709ce9
Julien Guanzon <julienguanzon@gmail.com>
2015-03-19 16:46:41 -0600

    Add PlanController to pull stationsList.
    Signed-off-by: Julien Guanzon <julienguanzon@gmail.com>

---
+++ GetMeUTA/PlanController.h
@@ -0,0 +1,15 @@
+//
+//  PlanController.h
+//  GetMeUTA
+//
+//  Created by Julien Guanzon on 3/19/15.
+//  Copyright (c) 2015 Shelley-Guanzon-Henrie-Mott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlanController : NSObject
+
+@property (nonatomic, strong) NSArray* stationsList;
+
+@end
---
+++ GetMeUTA/PlanController.m
@@ -0,0 +1,38 @@
+//
+//  PlanController.m
+//  GetMeUTA
+//
+//  Created by Julien Guanzon on 3/19/15.
+//  Copyright (c) 2015 Shelley-Guanzon-Henrie-Mott. All rights reserved.
+//
+
+#import "PlanController.h"
+#import <Parse/Parse.h>
+
+static NSString * const stopIDKey = @"stopID";
+static NSString * const stopNameKey = @"stopName";
+
+@implementation PlanController
+
+- (void)searchStationsForName {
+    
+    PFQuery *stationsQuery = [PFQuery queryWithClassName:@"stations"];
+    [stationsQuery selectKeys:@[@"stop_id", @"stop_name"]];
+    NSArray *stationsArray = [[NSArray alloc] initWithArray:[stationsQuery findObjects]];
+    for (PFObject *pfObject in stationsArray) {
+        [self addToDictionaryStopID:pfObject[@"stop_id"] stopName:pfObject[@"stop_name"]];
+    }
+}
+
+
+- (void)addToDictionaryStopID:(NSString *)stopID stopName:(NSString *)stopName {
+    NSMutableDictionary *dictionary = [NSMutableDictionary new];
+    [dictionary setValue:stopID forKey:stopIDKey];
+    [dictionary setValue:stopName forKey:stopNameKey];
+    
+    NSMutableArray *tempArray = [[NSMutableArray alloc] initWithArray:self.stationsList];
+    [tempArray addObject:dictionary];
+    self.stationsList = tempArray;
+}
+
+@end
